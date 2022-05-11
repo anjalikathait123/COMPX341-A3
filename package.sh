90ecbb8533efb2a35611387b1e4d1b8e9ce0667f
anjalikathait <anjalikathait9@gmail.com>
2022-05-12 11:23:35 +1200

    COMPX341-22A-A3 Committing from CI/CD Pipeline

--- package.sh
+++ package.sh
@@ -1,16 +1,14 @@
-#Going into the 'assets' directory
-#cd assets
-
-#https://github.com/anjalikathait123/COMPX341-A3.git
-
 #Committing any changes to Git
 git add .
 git commit -m "COMPX341-22A-A3 Committing from CI/CD Pipeline"
 git push origin master
 echo "Commit successful"
 
+#Going into the 'assets' directory
+cd assets
+
 #Running application using NPM
-#npm install
-#npm run build
-#npm run start
+npm install
+npm run build
+npm run start
 
